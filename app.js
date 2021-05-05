b5971397d74bbe1cdbbf56e7e31cd57d2fa8edc0
gPeckory <78902685+gPeckory@users.noreply.github.com>
2021-05-05 10:44:33 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-// // Declare Variable
-// const submitBtn = document.querySelector(".submit-btn");
 
-// // Add Event Listeners
-// submitBtn.addEventListener("click", function () {
-//   alert("Form Submitted, we will get back to you shortly!");
-// });

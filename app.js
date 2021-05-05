f8583ab1ed991be56dc2fa73a0be7e1962bf74ca
gPeckory <78902685+gPeckory@users.noreply.github.com>
2021-05-05 16:35:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+initMap = () => {
+  // Map options
+  const options = {
+    zoom: 15,
+    center: { lat: 51.548526, lng: 0.652211 },
+  };
 
+  //   New Map
+  const map = new google.maps.Map(document.getElementById("map"), options);
+
+  //   Add Marker
+  const marker = new google.maps.Marker({
+    position: { lat: 51.548526, lng: 0.652211 },
+    map: map,
+  });
+};
